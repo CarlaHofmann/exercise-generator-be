51d1e98aae299aa4a384f9dbf9909e89e8103af7
MatrixTheAssembler <52322925+MatrixTheAssembler@users.noreply.github.com>
2022-04-13 12:43:11 +0200

    added services

--- src/main/java/com/frauas/exercisegenerator/controllers/TestController.java
+++ src/main/java/com/frauas/exercisegenerator/controllers/TestController.java
@@ -1,13 +1,19 @@
 package com.frauas.exercisegenerator.controllers;
 
+import com.frauas.exercisegenerator.services.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private TestService testService;
+
+    public TestController(TestService testService){
+        this.testService = testService;
+    }
 
     @GetMapping(value = "/test")
     public String test(){
-        return "Hallo Welt";
+        return testService.test();
     }
 }
