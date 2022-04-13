0872f54812542417eac4bb861c83bf88f7c3e737
MatrixTheAssembler <52322925+MatrixTheAssembler@users.noreply.github.com>
2022-04-13 12:32:19 +0200

    initial commit

---
+++ src/main/java/com/frauas/exercisegenerator/ExerciseGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.frauas.exercisegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseGeneratorApplication.class, args);
+	}
+
+}
