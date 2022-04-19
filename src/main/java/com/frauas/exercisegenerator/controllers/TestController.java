868e938f098848fac3d01ba810be77d7f3804add
MatrixTheAssembler <lorenzo.petricone13@gmail.com>
2022-04-19 18:43:44 +0200

    added OpenAPI generator

--- src/main/java/com/frauas/exercisegenerator/controllers/TestController.java
+++ src/main/java/com/frauas/exercisegenerator/controllers/TestController.java
@@ -1,19 +1,31 @@
 package com.frauas.exercisegenerator.controllers;
 
 import com.frauas.exercisegenerator.services.TestService;
+import org.openapitools.api.TestApi;
+import org.openapitools.model.Book;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController implements TestApi {
     private TestService testService;
 
-    public TestController(TestService testService){
+    public TestController(TestService testService) {
         this.testService = testService;
     }
 
-    @GetMapping(value = "/test")
+    @GetMapping("beispiel")
     public String test(){
         return testService.test();
+    }
+
+    public ResponseEntity<String> getTest(){
+        return ResponseEntity.ok(testService.test());
+    }
+
+    public ResponseEntity<Book> getBook(@PathVariable("bookNumber") Integer bookNumber){
+        return ResponseEntity.ok(testService.getBook(bookNumber));
     }
 }
