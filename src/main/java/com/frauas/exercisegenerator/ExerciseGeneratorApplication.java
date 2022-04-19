b92dc1b891da8807ee1ee1ade1d8e9ceb46a91ec
MatrixTheAssembler <lorenzo.petricone13@gmail.com>
2022-04-19 18:46:18 +0200

    fixed CORS error

--- src/main/java/com/frauas/exercisegenerator/ExerciseGeneratorApplication.java
+++ src/main/java/com/frauas/exercisegenerator/ExerciseGeneratorApplication.java
@@ -2,6 +2,9 @@ package com.frauas.exercisegenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExerciseGeneratorApplication {
@@ -10,4 +13,16 @@ public class ExerciseGeneratorApplication {
 		SpringApplication.run(ExerciseGeneratorApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*");
+			}
+		};
+	}
 }
